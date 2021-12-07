c627553a4194133b669cb419fe6f5d26e4d57336
Shinichi Okada <okada.shin@gmail.com>
2021-12-07 21:33:27 +0900

    Index update.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let name = "world";
+</script>
+
+<input bind:value={name} />
+
+<h1>Hello {name}!</h1>
