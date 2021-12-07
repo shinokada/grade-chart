06042b8ad6d42eba65cefd25702991e8beffec24
Shinichi Okada <okada.shin@gmail.com>
2021-12-07 21:19:55 +0900

    Added Sveltekit and vercel adapter

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: vercel(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
